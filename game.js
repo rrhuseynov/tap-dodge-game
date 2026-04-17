c143a0b0372bce9f89a544567c8b5e88d3de1470
rrhuseynov <91523307+rrhuseynov@users.noreply.github.com>
2026-04-17 14:17:15 +0400

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,26 @@
-function startGame() {
-  alert("Game started!");
+function show(id) {
+  document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
 }
+
+/* welcome → menu */
+setTimeout(() => {
+  show('menu');
+}, 2000);
+
+/* показать continue */
+function showContinue() {
+  document.getElementById('continueBox').classList.remove('hidden');
+}
+
+/* скрыть continue */
+function hideContinue() {
+  document.getElementById('continueBox').classList.add('hidden');
+}
+
+/* ДЛЯ ТЕСТА (пока нет физики) */
+setTimeout(() => {
+  if (document.getElementById('game').classList.contains('active')) {
+    showContinue();
+  }
+}, 5000);
